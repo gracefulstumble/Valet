4ca6359929be31e787a416dedd0dc1bb9985979a
Emily Gonzales <emilyg89503@gmail.com>
2021-06-25 21:12:33 -0700

    functional components

--- Desktop/NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
 import { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg,  CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
-    renderCampsite(campsite){
+
+ function RenderCampsite(campsite){
         return(
             <div className="col-md-5 m-1">
                 <card>
@@ -14,9 +14,10 @@ class CampsiteInfo extends Component {
                     </cardBody>
                 </card>
             </div>
-        );
-    }
-    renderComments(comments) {
+        )
+    
+        }
+ function renderComments(comments) {
         if (comments) {
             return(
                 <div className="col-md-5 m-1">
@@ -30,22 +31,22 @@ class CampsiteInfo extends Component {
                         )
                     })}
                 </div>
-            )
+            );
         }
-    }
-    render(){
-        if (this.props.campsite){
-            return(
+    };
+function CampsiteInfo(props) {
+        if (props.campsite) {
+            return (
                 <div className="container">
                     <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
                     </div>
                 </div>
             );
-        } else {
-            return (<div></div>);
         }
-    };
-}
+        return <div />;
+    }
+
+
 export default CampsiteInfo;
