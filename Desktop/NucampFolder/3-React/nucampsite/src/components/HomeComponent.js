648d8fa19db0d93e98b971a497b1c562a14c2645
Emily Gonzales <emilyg89503@gmail.com>
2021-07-10 08:56:11 -0700

    fetch from server

--- Desktop/NucampFolder/3-React/nucampsite/src/components/HomeComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Loading } from './LoadingComponent';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) {
@@ -11,7 +12,7 @@ function RenderCard({item, isLoading, errMess}) {
     }
     return (
         <Card>
-            <CardImg src={item.image} alt={item.name} />
+            <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 <CardText>{item.description}</CardText>
@@ -26,16 +27,24 @@ function Home(props) {
             <div className="row">
                 <div className="col-md m-1">
                     <RenderCard
-                        item={props.campsite}
-                        isLoading={props.campsitesLoading}
-                        errMess={props.campsitesErrMess}
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
                     />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
+                    <RenderCard
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    />
                 </div>
             </div>
         </div>
