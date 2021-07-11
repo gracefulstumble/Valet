8fbf9405f16da6b7517830105352f997bb5f8c10
Emily Gonzales <emilyg89503@gmail.com>
2021-07-11 12:00:07 -0700

    wk5 debug

--- Desktop/NucampFolder/3-React/nucampsite/src/components/ContactComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/ContactComponent.js
@@ -4,6 +4,7 @@ import { Breadcrumb, BreadcrumbItem,
 import { Link } from 'react-router-dom';
 import { Control, Form, Errors } from 'react-redux-form';
 
+
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -37,9 +38,9 @@ class Contact extends Component {
 
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.postFeedback(values);
         this.props.resetFeedbackForm();
+
     }
 
     render() { 
